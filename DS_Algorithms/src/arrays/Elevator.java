22cc752c0d17da6ca7f5576414f504ccf60cfd1e
Sunitha Prabhu <sunitha.prabhu@gmail.com>
2017-01-16 00:50:33 -0800

    test

--- DS_Algorithms/src/arrays/Elevator.java
+++ DS_Algorithms/src/arrays/Elevator.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-
+ 
 class Elevator {
     public int solution(int[] A, int[] B, int M, int X, int Y) {
         

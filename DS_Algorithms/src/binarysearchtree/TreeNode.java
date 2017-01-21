27445126df63916ce93248cc9e0b2e2867fed7c3
Sunitha Prabhu <sunitha.prabhu@gmail.com>
2017-01-20 21:48:11 -0800

    treenode

---
+++ DS_Algorithms/src/binarysearchtree/TreeNode.java
@@ -0,0 +1,13 @@
+package binarysearchtree;
+
+public  class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode parent;
+    TreeNode(int x) { val = x; }
+    
+    public String toString(){
+    	return val+""; 
+    }
+}
